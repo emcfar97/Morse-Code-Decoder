5433ea27411e903bae37567cf1180f8598e8b090
at8nd <taylo127@miamioh.edu>
2019-04-26 09:46:59 -0500

    .

--- Project 2 - Morse Code Decoder/Source.cpp
+++ Project 2 - Morse Code Decoder/Source.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "morseCode.h"
 using namespace std;
 
 int main() {
+	ofstream;
 	string input = "shsftghsh";
 	morseCode encoding = morseCode(input);
 	string decoded = encoding.decode();
