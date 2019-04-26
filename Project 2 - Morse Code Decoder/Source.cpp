2c5610ae666a1f3f030230810ed8b4b23c446866
1stRayos <emc1140@hotmail.com>
2019-04-26 09:22:25 -0500

    .

--- Project 2 - Morse Code Decoder/Source.cpp
+++ Project 2 - Morse Code Decoder/Source.cpp
@@ -4,20 +4,7 @@
 using namespace std;
 
 int main() {
-
-	while (true) {
-
-		std::string input;
-		cout << "Enter word to endode: ";
-		cin >> input;
-
-		morseCode encoding = morseCode(input);
-		cout << encoding.decode() << endl;
-
-		cout << "Do you want to encode another word?\nEnter 'y' or 'n'";
-		cin >> input;
-
-		if (input == "y") continue;
-		else if (input == "n") break;
-	}
+	string input = "";
+	morseCode encoding = morseCode(input);
+	string decoded = encoding.decode();
 }
