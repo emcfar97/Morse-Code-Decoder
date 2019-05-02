1632849ba92117d4fc86409343776a50a78eb83f
nxs4z3 <ninjaninax@gmail.com>
2019-05-02 13:57:25 -0500

    edited encode bc some of the morse code was not associated with the correct alphabet also removed space at end of morse code value and had the for loop add the space between the letters

--- Project 2 - Morse Code Decoder/morseCode.cpp
+++ Project 2 - Morse Code Decoder/morseCode.cpp
@@ -23,34 +23,36 @@ std::string morseCode::encode(std::string userStr)
 	std::string encoding;
 	std::map<char, std::string> morse;
 
-	morse['a'] = "._ ";
-	morse['b'] = "_... ";
-	morse['c'] = "_._ ";
-	morse['e'] = "_.. ";
-	morse['f'] = ". ";
-	morse['g'] = ".._. ";
-	morse['h'] = ".... ";
-	morse['i'] = ".. ";
-	morse['j'] = ".____ ";
-	morse['k'] = "_._ ";
-	morse['l'] = "._.. ";
-	morse['m'] = "__ ";
-	morse['n'] = "_. ";
-	morse['o'] = "___ ";
-	morse['p'] = ".__. ";
-	morse['q'] = "__._ ";
-	morse['r'] = "._. ";
-	morse['s'] = "... ";
-	morse['t'] = "_ ";
-	morse['u'] = ".._ ";
-	morse['v'] = "..._ ";
-	morse['w'] = ".__ ";
-	morse['x'] = "_.._ ";
-	morse['y'] = "_.__ ";
-	morse['z'] = "__.. ";
-	
+	morse['a'] = "._";
+	morse['b'] = "_...";
+	morse['c'] = "_._.";
+	morse['d'] = "_..";
+	morse['e'] = ".";
+	morse['f'] = ".._.";
+	morse['g'] = "__.";
+	morse['h'] = "....";
+	morse['i'] = "..";
+	morse['j'] = ".___";
+	morse['k'] = "_._";
+	morse['l'] = "._..";
+	morse['m'] = "__";
+	morse['n'] = "_.";
+	morse['o'] = "___";
+	morse['p'] = ".__.";
+	morse['q'] = "__._";
+	morse['r'] = "._.";
+	morse['s'] = "...";
+	morse['t'] = "_";
+	morse['u'] = ".._";
+	morse['v'] = "..._";
+	morse['w'] = ".__";
+	morse['x'] = "_.._";
+	morse['y'] = "_.__";
+	morse['z'] = "__..";
+
 	for (std::string::iterator it = userStr.begin(); it != userStr.end(); ++it) {
 		encoding += morse.find(*it)->second;
+		encoding += " ";
 	}
 	return encoding;
 }
